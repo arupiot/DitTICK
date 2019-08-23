83c3456af563eca48a24668f98a9482284caf2b0
InBrewJ <jason.brewer@arupiot.com>
2019-08-23 22:20:29 +0100

    Added telegraf config and grafana dashboard config for the UDMIduino set up

---
+++ TICK/get_repos.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/artlov/docker-telegraf-influxdb-grafana.git
+git clone https://github.com/philhawthorne/docker-influxdb-grafana.git
+git clone https://github.com/samuelebistoletti/docker-statsd-influxdb-grafana.git
